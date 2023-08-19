2e08dcfab17a530a1c9b1d16c2b456da3194f763
KMG <rlaaudrb1104@gmail.com>
2023-08-19 17:15:22 +0900

    크롤러만드는법

--- crawler.py
+++ crawler.py
@@ -11,7 +11,8 @@ print(data.status_code) #웹페이지 정상접속 유무 200>>정상 400or500>>
 soup = BeautifulSoup(data.content, 'html.parser') #가져온 데이터 가공
 
 print(soup)#가공된 데이터 출력
-print(soup.find_all('strong',id="_nowVal")[0].text) #필요한 정보만 출력 ('태그명',"속성명") // 찾은 결과 리스트로 반환
+print('현재가:' + soup.find_all('strong',id="_nowVal")[0].text) 
+#필요한 정보만 출력 ('태그명',"속성명") // 찾은 결과 리스트로 반환
 
 print('거래량:' + soup.find_all('span', id="_quant")[0].text) 
 #거래량 출력// 클래스로 찾고 싶으면 class_= // 띄어쓰기 되어있을 시 하나만 쓴다.
